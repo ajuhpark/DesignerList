14a0307fac2e39489afc824151770fa4639d7d17
Brian Fang <fangbrian@gmail.com>
2017-09-21 22:25:54 -0700

    Added a custom adapter

---
+++ app/src/main/java/com/andrew/designerlist/models/Designer.java
@@ -0,0 +1,17 @@
+package com.andrew.designerlist.models;
+
+/**
+ * Created by fangbrian on 9/21/17.
+ */
+
+public class Designer {
+    private String name;
+
+    public Designer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- app/src/main/java/com/andrew/designerlist/views/MainActivity.java
+++ app/src/main/java/com/andrew/designerlist/views/MainActivity.java
@@ -1,4 +1,4 @@
-package com.andrew.designerlist;
+package com.andrew.designerlist.views;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,7 +9,16 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.andrew.designerlist.R;
+import com.andrew.designerlist.adapters.DesignerAdapter;
+import com.andrew.designerlist.models.Designer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
+
+    private DesignerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +29,6 @@ public class MainActivity extends Activity {
         Create an array
         */
 
-        String[] designers = {"Robert Geller", "Native Son", "Ace & Jig", "The Row", "Lemaire", "Siki Im"};
-
         /*
         We need an adapter that's responsible for conversion from
         java code (the array) to list items that I can use.
@@ -30,13 +37,13 @@ public class MainActivity extends Activity {
         android.R.layout.simple_list_items_1. We're using simple
         list items 1 for text list.
          */
-        ListAdapter designersAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, designers);
 
+        adapter = new DesignerAdapter(this, R.layout.designer_list_item, populateDesignerList());
         /*
         create a reference to the list
          */
         ListView designersListView = (ListView) findViewById(R.id.designersListView);
-        designersListView.setAdapter(designersAdapter);
+        designersListView.setAdapter(adapter);
 
         designersListView.setOnItemClickListener(
                 new AdapterView.OnItemClickListener(){
@@ -49,6 +56,16 @@ public class MainActivity extends Activity {
                 }
         );
 
+    }
+
+    private List<Designer> populateDesignerList() {
+        String[] designersStringArray = {"Robert Geller", "Native Son", "Ace & Jig", "The Row", "Lemaire", "Siki Im"};
+        List<Designer> designers = new ArrayList<Designer>();
+        for (int i = 0; i < designersStringArray.length; i++) {
+            designers.add(new Designer(designersStringArray[i]));
+        }
+
+        return designers;
     }
 }
 
